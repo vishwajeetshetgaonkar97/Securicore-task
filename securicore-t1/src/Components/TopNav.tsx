3ca13961e5c4f458b63288f6e44600100864e607
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-06-18 22:09:33 -0400

    fix

--- securicore-t1/src/Components/TopNav.tsx
+++ securicore-t1/src/Components/TopNav.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { createPortal } from 'react-dom'
 import LogoDark from '../assets/Securicore_White.svg'
