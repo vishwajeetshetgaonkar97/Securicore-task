6a0e26050e4c4e449d94038a2750afe44b8db262
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-06-19 01:10:25 -0400

    try 9

--- securicore-t1/src/Pages/Home.tsx
+++ securicore-t1/src/Pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-// import TopNav from '../Components/TopNav';
+import TopNav from '../Components/TopNav';
 import EditIcon from '../assets/editIcon.png'
 import DeleteIcon from '../assets/deleteIcon.png'
 
@@ -9,9 +9,12 @@ interface User {
     email: string;
 }
 
+interface HomeProps {
+    dark: boolean;
+    toggleDark: () => void;
+}
 
-
-export default function Home() {
+export default function Home({ dark, toggleDark }: HomeProps) {
     const [users, setUsers] = useState<User[]>([
         { id: 1, name: 'John Doe', email: 'john@example.com' },
         { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
@@ -92,7 +95,7 @@ export default function Home() {
 
     return (
         <div className='min-h-screen w-screen overflow-x-clip bg-white text-black dark:bg-neutral-950 dark:text-white transition transition-colors duration-300'>
-            {/* <TopNav dark={dark} toggleDark={toggleDark} /> */}
+           { false && <TopNav dark={dark} toggleDark={toggleDark} />}
 
             <div className="relative  h-full w-full bg-slate-950 opacity-0 dark:opacity-100">
                 <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
