db005d3f18180cd467f2d89afe8a64e41b65c2f8
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-06-19 00:53:43 -0400

    try

--- securicore-t1/src/Pages/Home.tsx
+++ securicore-t1/src/Pages/Home.tsx
@@ -97,7 +97,7 @@ export default function Home({ dark, toggleDark }: HomeProps) {
         <div className='min-h-screen w-screen overflow-x-clip bg-white text-black dark:bg-neutral-950 dark:text-white transition-colors duration-300'>
             <TopNav dark={dark} toggleDark={toggleDark} />
 
-            <div className="relative h-full w-screen bg-slate-950 opacity-0 dark:opacity-100">
+            <div className="relative h-full w-full bg-slate-950 opacity-0 dark:opacity-100">
                 <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
                 <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
             </div>
