b39ca6c8a76f7f542a72fe576baab345781ff83f
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-06-19 01:02:55 -0400

    try 5

--- securicore-t1/src/Pages/Home.tsx
+++ securicore-t1/src/Pages/Home.tsx
@@ -97,12 +97,12 @@ export default function Home({ dark, toggleDark }: HomeProps) {
         <div className='min-h-screen w-screen overflow-x-hidden bg-white text-black dark:bg-neutral-950 dark:text-white transition-colors duration-300'>
             <TopNav dark={dark} toggleDark={toggleDark} />
 
-            <div className="relative  h-full w-full bg-slate-950 opacity-0 dark:opacity-100">
+            <div className="relative hidden h-full w-full bg-slate-950 opacity-0 dark:opacity-100">
                 <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
                 <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
             </div>
 
-            <div className="p-4 md:p-8">
+            <div className="p-4 hidden md:p-8">
                 <div className='flex  justify-between items-end gap-2 sm:gap-0 flex-col sm:flex-row'>
                     <div className='w-full'>
                         <h3 className="text-2xl font-bold uppercase opacity-80 dark:opacity-90">Welcome!</h3>
