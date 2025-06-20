8bd10654261276c60e014c68a1fb6f1bbc603307
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2025-06-20 01:17:20 -0400

    minor

--- securicore-t1/src/Components/TopNav.tsx
+++ securicore-t1/src/Components/TopNav.tsx
@@ -45,7 +45,7 @@ export default function TopNav({ dark, toggleDark }: TopNavProps) {
                         </button>
                         <button 
                             onClick={handleConfirmLogout} 
-                            className="px-4 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+                            className="px-4 py-1 bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer"
                         >
                             Logout
                         </button>
